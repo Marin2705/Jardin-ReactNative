f07f1d5af6b2887172087a8714492827793260c4
Marin2705 <marin.bouanchaud@gmail.com>
2022-02-16 16:31:26 +0100

    feat(homepage): add header and image

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View, Image } from 'react-native'
+
+console.log('Console hello world !')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>Jardins du Luxembourg</Text>
+      <Image
+        style={styles.headerImage}
+        source={require('./assets/accueil.jpg')}
+      />
+      {/* <StatusBar style="auto" /> */}
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: '100%',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
   },
-});
+  header: {
+    margin: 20,
+    fontSize: 40,
+    alignItems: 'flex-start',
+    width: '100%',
+  },
+  headerImage: {
+    height: '30%',
+    width: '100%',
+  },
+})
