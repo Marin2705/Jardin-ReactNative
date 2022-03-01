2dde0e34f30ca7cc21cd8f45243ada88f2601839
Lucien77144 <79203857+Lucien77144@users.noreply.github.com>
2022-03-01 14:41:58 +0100

    fix(header): avancement positionnement header

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import Nav from './component/Header'
 import Home from './component/Home'
 import AppLoading from 'expo-app-loading'
 import { useFonts } from 'expo-font'
+import { backgroundColor, borderBottomColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes'
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -16,20 +17,24 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <Home style={styles.home}/>
       <View style={styles.nav}>
-        <Nav style={styles.nav}/>
+        <Nav/>
       </View>
-      <Home/>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
+  },
+  home: {
     flex: 1
   },
   nav: {
-    
+    position: 'absolute',
+    backgroundColor: 'red',
+    bottom: 0
   }
 
 })
