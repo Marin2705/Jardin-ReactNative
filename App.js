dee352a806efe57d871e095462a94f51fc85cc78
Lucien77144 <79203857+Lucien77144@users.noreply.github.com>
2022-03-11 08:27:13 +0100

    menu

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
 import Home from './component/Home'
 import Test from './component/Test'
 import Surroundings from './component/Surroundings'
@@ -24,12 +24,13 @@ export default function App() {
     <View style={styles.container}>
       <NavigationContainer>
         <Tab.Navigator
+          tabBar={(props) => <MyTabBar {...props} />}
           screenOptions={{
             headerShown: false,
           }}
           initialRouteName="Home"
         >
-          <Tab.Screen name="Home" component={Home} />
+          <Tab.Screen name="Home" component={Home}/>
           <Tab.Screen name="Surroundings" component={Surroundings} />
           <Tab.Screen name="Test" component={Test} />
         </Tab.Navigator>
@@ -43,3 +44,55 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 })
+
+function MyTabBar({ state, descriptors, navigation }) {
+  return (
+    <View style={{ flexDirection: 'row' }}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacity
+            accessibilityRole="button"
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{ flex: 1 }}
+          >
+            <Text style={{ color: isFocused ? '#673ab7' : '#222' }}>
+              {label}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
+}
