f099eb2e64f41c81a3b287f749f468636edfc30a
Marin2705 <marin.bouanchaud@gmail.com>
2022-03-23 12:14:57 +0100

    feat(NewEvent): center on new marker

--- component/NewEvent.js
+++ component/NewEvent.js
@@ -12,7 +12,6 @@ import MapContainer from './MapContainer'
 
 // Accepter la fonction childToParent passée en prop
 function NewEvent({ childToParent }) {
-
   // BOX SHADOW QUI FONCTIONNE SEULEMENT SUR MOBILE ???
   // generateBoxShadowStyle(-2, 4, '#000000', 0.25, 3, 4, '#000000');
 
@@ -20,82 +19,107 @@ function NewEvent({ childToParent }) {
   const [name, setName] = useState('')
   const [description, setDescription] = useState('')
   const [address, setAddress] = useState([2.337179, 48.846836])
-  const apitoken = "pk.eyJ1IjoiZWxvcG4iLCJhIjoiY2t3dnRsNm5zMjNwcTJ3cDNjdjB1cWNldCJ9.tH301UpWotwarxl8l7w9HA"
+  const apitoken =
+    'pk.eyJ1IjoiZWxvcG4iLCJhIjoiY2t3dnRsNm5zMjNwcTJ3cDNjdjB1cWNldCJ9.tH301UpWotwarxl8l7w9HA'
 
+  let timeout
   const getCoord = (input) => {
-      fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/" + input + ".json?access_token=" + apitoken).then(response => {
-          response.json().then(value => {
-              setAddress(value.features[0].center)
+    clearTimeout(timeout)
+    timeout = setTimeout(() => {
+      if (input) {
+        fetch(
+          'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
+            input +
+            '.json?access_token=' +
+            apitoken
+        ).then((response) => {
+          response.json().then((value) => {
+            setAddress(value.features[0].center)
           })
-      })
+        })
+      }
+    }, 600)
   }
 
-    const onSubmit = () => {
-        let data = { name: name, description: description, address: address }
+  const onSubmit = () => {
+    let data = { name: name, description: description, address: address }
 
-        fetch('http://172.24.141.205/reactnative/Jardin-ReactNative/assets/api/Surroundings.php?action=addEvent',
-        {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            method: "POST",
-            body: JSON.stringify(data)
-        })
-        .then(() => {
-            childToParent();
-        })
-    }
+    fetch(
+      'http://172.24.141.205/reactnative/Jardin-ReactNative/assets/api/Surroundings.php?action=addEvent',
+      {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body: JSON.stringify(data),
+      }
+    ).then(() => {
+      childToParent()
+    })
+  }
 
-    return (
-        <View style={[styles.popup, styles.boxShadow]}>
+  return (
+    <View style={[styles.popup, styles.boxShadow]}>
+      {/* Drag vers le bas -> déclencher l'évènement childToParent qui sera détectée par le component parent */}
+      <Pressable
+        style={{ paddingVertical: 15 }}
+        onTouchStart={(e) => setTouchY(e.nativeEvent.pageY)}
+        onTouchEnd={(e) => {
+          if (e.nativeEvent.pageY - touchY > 20) {
+            childToParent()
+          }
+        }}
+      >
+        <View style={styles.handle}></View>
+      </Pressable>
 
-            {/* Drag vers le bas -> déclencher l'évènement childToParent qui sera détectée par le component parent */}
-            <Pressable style={{paddingVertical: 15}}
-            onTouchStart={e=> setTouchY(e.nativeEvent.pageY)}
-            onTouchEnd={e => {
-                if (e.nativeEvent.pageY - touchY > 20) {
-                    childToParent()
-                }
-            }}>
-                <View style={styles.handle}></View>
-            </Pressable>
+      <KeyboardAvoidingView behavior="height">
+        <ScrollView style={styles.form}>
+          <View>
+            <Text>Nom</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Nom du lieu"
+              onChangeText={(insertedName) => setName(insertedName)}
+              defaultValue={name}
+            />
+          </View>
+          <View>
+            <Text>Description</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Description du lieu"
+              onChangeText={(insertedDesc) => setDescription(insertedDesc)}
+              defaultValue={description}
+            />
+          </View>
+          <View>
+            <Text>Adresse</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Adresse du lieu"
+              onChangeText={(insertedAddress) => getCoord(insertedAddress)}
+              defaultValue={address}
+            />
+          </View>
+          <View style={styles.map}>
+            {/* Localisation correspondant à l'adresse saisie */}
+            <MapContainer
+              lat={address[1]}
+              long={address[0]}
+              name={name}
+              description={description}
+            />
+          </View>
 
-            <KeyboardAvoidingView  behavior="height">
-                <ScrollView style={styles.form}>
-                    <View>
-                        <Text>Nom</Text>
-                        <TextInput style={styles.input}
-                        placeholder="Nom du lieu"
-                        onChangeText={insertedName => setName(insertedName)}
-                        defaultValue={name} />
-                    </View>
-                    <View>
-                        <Text>Description</Text>
-                        <TextInput style={styles.input}
-                        placeholder="Description du lieu"
-                        onChangeText={insertedDesc => setDescription(insertedDesc)}
-                        defaultValue={description} />
-                    </View>
-                    <View>
-                        <Text>Adresse</Text>
-                        <TextInput style={styles.input}
-                        placeholder="Adresse du lieu"
-                        onChangeText={insertedAddress => getCoord(insertedAddress)}
-                        defaultValue={address} />
-                    </View>
-                    <View style={styles.map}>
-                        {/* Localisation correspondant à l'adresse saisie */}
-                        <MapContainer lat={address[1]} long={address[0]} name={name} description={description} />
-                    </View>
-                
-                    <Pressable style={styles.button} onPress={onSubmit}>
-                        <Text style={styles.textButton}>Partager</Text>
-                    </Pressable>
-                </ScrollView> 
-            </KeyboardAvoidingView>
-        </View>
-    )
+          <Pressable style={styles.button} onPress={onSubmit}>
+            <Text style={styles.textButton}>Partager</Text>
+          </Pressable>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </View>
+  )
 }
 
 // Box Shadow en fonction de l'OS de l'appareil (SEULEMENT SUR MOBILE ??)
