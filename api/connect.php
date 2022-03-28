36f13070b983444de0843c250fd79ab3543d2e3e
Marin2705 <marin.bouanchaud@gmail.com>
2022-03-28 12:02:48 +0200

    fix(build) : add API key for google maps

--- api/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $host = "sqletud.u-pem.fr";
-    $dbname = "elodie.pan_db";
-    $login = "elodie.pan";
-    $password = "elodie";
-    
-    $db = new PDO("mysql:host=$host;dbname=$dbname;port=3306;charset=utf8", $login, $password);
-?>
